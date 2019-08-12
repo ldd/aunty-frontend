28cc8c30272530134acbf535bcd343774ba25d10
ldd <leonardoldd@gmail.com>
2019-08-12 18:46:01 -0400

    feat(Login): add new page for Login (e.g: slack OAuth flow)

--- src/App.js
+++ src/App.js
@@ -6,18 +6,24 @@ import Home from "./components/Home";
 import Pricing from "./components/Pricing";
 import Integrations from "./components/Integrations";
 import Tasks from "./components/Tasks";
+import Login from "./components/Login";
 
-const pages = ["Home", "Pricing", "Integrations", "Tasks"];
+const pages = ["Home", "Pricing", "Integrations", "Tasks", "Login"];
+
+const parseRoute = route =>
+  route === "" ? window.location.hash.slice(1) : route;
 
 const Router = ({ route }) => {
-  if (route === "Pricing") return <Pricing />;
-  else if (route === "Integrations") return <Integrations />;
-  else if (route === "Tasks") return <Tasks />;
+  const parsedRoute = parseRoute(route);
+  if (parsedRoute === "Pricing") return <Pricing />;
+  else if (parsedRoute === "Integrations") return <Integrations />;
+  else if (parsedRoute === "Tasks") return <Tasks />;
+  else if (parsedRoute === "Login") return <Login />;
   return <Home />;
 };
 
 const App = () => {
-  const [route, changeRoute] = useState("home");
+  const [route, changeRoute] = useState("");
   return (
     <section className="hero is-success is-fullheight is-bold">
       <Navbar route={route} changeRoute={changeRoute} pages={pages} />
---
+++ src/components/Login/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const BACKEND_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://lddstudios.com/api"
+    : "http://localhost:4000";
+
+const SLACK_CLIENT_ID = "695399984727.687207103297";
+
+const SLACK_REDIRECT_URI = encodeURI(`${BACKEND_URL}/auth/slack`);
+
+const Login = () => (
+  <div className="hero-body">
+    <div className="container has-text-centered">
+      <div className="columns">
+        <div className="column is-narrow">
+          {/* from slack's button generator https://api.slack.com/docs/sign-in-with-slack#generator */}
+          <a
+            href={`https://slack.com/oauth/authorize?scope=identity.basic&client_id=${SLACK_CLIENT_ID}&redirect_uri=${SLACK_REDIRECT_URI}`}
+          >
+            <img
+              alt="Sign in with Slack"
+              height="40"
+              width="172"
+              src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
+              srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Login;
