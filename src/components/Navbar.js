71b3a75ee56e25ff2501c736ed6fd21dc785a9d9
ldd <leonardoldd@gmail.com>
2019-08-27 19:45:06 -0400

    feat(navbar): toggle burger menu on mobile devices

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,48 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
-const Navbar = ({ route, changeRoute, pages }) => (
-  <div className="hero-head">
-    <header className="navbar">
-      <div className="container">
-        <div className="navbar-brand">
-          {/* <a className="navbar-item" href="#">
-            <img
-              src="https://bulma.io/images/bulma-type-white.png"
-              alt="Logo"
-            />
-          </a> */}
-          <span className="navbar-burger burger" data-target="navbarMenuHeroC">
-            {/* needed to simulate a burger menu */}
-            <span />
-            <span />
-            <span />
-          </span>
-        </div>
-        <div id="navbarMenuHeroC" className="navbar-menu">
-          <div className="navbar-end">
-            {pages.map(link => (
-              <a
-                key={`navbar-${link}`}
-                href={`#${link}`}
-                onClick={() => changeRoute(link)}
-                className={`navbar-item ${link === route ? "is-active" : ""}`}
-              >
-                {link}
-              </a>
-            ))}
+const Navbar = ({ route, changeRoute, pages }) => {
+  const [menuStatus, toggleMenuStatus] = useState("");
+
+  const menuHandler = useCallback(() => {
+    toggleMenuStatus(menuStatus === "" ? "is-active" : "");
+  }, [toggleMenuStatus, menuStatus, route]);
+
+  const generateChangeRoute = link => () => {
+    toggleMenuStatus("");
+    changeRoute(link);
+  };
+  return (
+    <div className="hero-head">
+      <header className="navbar">
+        <div className="container">
+          <div className="navbar-brand" onClick={menuHandler}>
+            <span
+              className={`navbar-burger burger ${menuStatus}`}
+              data-target="navbarMenuHeroC"
+            >
+              {/* needed to simulate a burger menu */}
+              <span />
+              <span />
+              <span />
+            </span>
+          </div>
+          <div id="navbarMenuHeroC" className={`navbar-menu ${menuStatus}`}>
+            <div className="navbar-end">
+              {pages.map(link => (
+                <a
+                  key={`navbar-${link}`}
+                  href={`#${link}`}
+                  onClick={generateChangeRoute(link)}
+                  className={`navbar-item ${link === route ? "is-active" : ""}`}
+                >
+                  {link}
+                </a>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    </header>
-  </div>
-);
-
+      </header>
+    </div>
+  );
+};
 export default Navbar;
